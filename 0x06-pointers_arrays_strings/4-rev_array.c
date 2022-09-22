652c69980d3605147e67dca2026bd1352c57e8bd
Ademez <ademolaoladosu1@gmail.com>
2022-09-21 19:18:57 -0700

    my commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverse the content of an array of integers
+ * @a: array variable
+ * @n: Number of element of array
+ *
+ * Return: void
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, rev;
+
+	for (i = n - 1; i >= n / 2; i--)
+	{
+		rev = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = rev;
+	}
+}
