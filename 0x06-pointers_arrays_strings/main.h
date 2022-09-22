68b3f0bfafb06912e64bfc3206334eaee9350c91
Ademez <ademolaoladosu1@gmail.com>
2022-09-22 07:57:04 -0700

    my commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*bwave*/
+
 char *_strcat(char *dest, char *src);
 
 int _putchar(char c);
@@ -23,4 +25,8 @@ char *rot13(char *);
 
 void print_number(int n);
 
-#endif 
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
+#endif /*Bwave*/
