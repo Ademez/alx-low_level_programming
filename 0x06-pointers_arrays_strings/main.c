28a124afddde8d05de592412b64712cb5a3b04f0
Ademez <ademolaoladosu1@gmail.com>
2022-09-22 06:03:43 -0700

    my commit

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -1,23 +1,33 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 
 int main(void)
-{
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
-	char *ptr;
 
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strcat(s1, s2);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
+{
+	print_number(98);
+
+	_putchar('\n');
+
+	print_number(402);
+
+	_putchar('\n');
+
+	print_number(1024);
+
+	_putchar('\n');
+
+	print_number(0);
+
+	_putchar('\n');
+
+	print_number(-98);
+
+	_putchar('\n');
+
 	return (0);
 }
